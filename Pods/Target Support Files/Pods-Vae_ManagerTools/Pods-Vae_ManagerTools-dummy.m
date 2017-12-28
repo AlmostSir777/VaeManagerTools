051b71a835e6d2b4a480aaa048b4becc9ef44b87
yuhuimin <m13661414919@163.com>
2017-12-28 15:34:45 +0800

    new target to pull my collect OC tools
    new target to pull my collect OC tools

---
+++ Pods/Target Support Files/Pods-Vae_ManagerTools/Pods-Vae_ManagerTools-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Vae_ManagerTools : NSObject
+@end
+@implementation PodsDummy_Pods_Vae_ManagerTools
+@end
---
+++ Vae_ManagerTools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Vae_ManagerTools
+//
+//  Created by 闵玉辉 on 2017/12/28.
+//  Copyright © 2017年 闵玉辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
